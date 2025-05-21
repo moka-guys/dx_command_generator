fe420f491ad7704a2094c4fbba8c8dd9f1dc68d6
George Doyle <gdoyle046@gmail.com>
2025-05-21 14:07:43 +0100

    more hardcoding removal

--- modules/readcount.py
+++ modules/readcount.py
@@ -15,7 +15,7 @@ class ReadcountCommandGenerator(DXCommandGenerator):
         self.readcount_applet_id = self.config_values.get('readcount_applet')
         self.reference_genome = self.config_values.get('reference_genome')
         self.normals_RData = self.config_values.get('normals_RData')
-        self.common_data_project = self.config_values.get('common_data_project') # Used for bedfile path
+        self.readcount_bedfile = self.config_values.get('readcount_bedfile')
 
     @property
     def name(self) -> str:
@@ -55,9 +55,6 @@ class ReadcountCommandGenerator(DXCommandGenerator):
                 print("Error: Could not extract PAN numbers from the manifest file.")
                 return
 
-            # auth_token is now retrieved inside _initialize_output_file if include_project_vars is True
-            # or directly using self._get_auth_token()
-
             pan_numlist = ",".join(sorted(pan_numbers))
             output_filename = f"{project_name.replace(' ', '_')}_readcount_cmd.sh"
 
@@ -70,7 +67,7 @@ class ReadcountCommandGenerator(DXCommandGenerator):
                     command = (
                         f"dx run {self.readcount_applet_id} --priority high -y --instance-type mem1_ssd1_v2_x8 --name \"ED_Readcount-CP2\" "
                         f"-ireference_genome={self.reference_genome} "
-                        f"-ibedfile={self.common_data_project}:/Data/BED/Pan5279_exomeDepth.bed " # Hardcoded BED, but project ID from config
+                        f"-ibedfile={self.readcount_bedfile} "
                         f"-ibam_str=\"*markdup.ba*\" "
                         f"-inormals_RData={self.normals_RData} "
                         f"-iproject_name=\"${{PROJECT_NAME}}\" " # Use shell variable
